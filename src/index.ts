deb18204cedc50ca2ba5993830909fab4266a64b
Furkan Toprak <furkancemaltoprak@gmail.com>
2020-07-18 20:25:26 -0500

    Expressed it.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('Hello world!');
+/* eslint-disable no-undef */
+import express from 'express';
+
+const app = express();
+const port = 3000;
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+app.listen(port, err => {
+  if (err) {
+    return console.error(err);
+  }
+  return console.log(`server is listening on ${port}`);
+});
